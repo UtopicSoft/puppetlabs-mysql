de14e9dd397cc6744027d6acaa5b8bf1a307982c
UtopicSoft <tc@n-ix.com>
2018-11-23 15:46:41 +0200

    Update install.pp

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -6,7 +6,12 @@
 class mysql::server::install {
 
   if $mysql::server::package_manage {
-
+  
+    #+
+    #+ @name: mysql-server
+    #+ @type: package
+    #+ @description: MySQL server package.
+    #+
     package { 'mysql-server':
       ensure          => $mysql::server::package_ensure,
       install_options => $mysql::server::install_options,
